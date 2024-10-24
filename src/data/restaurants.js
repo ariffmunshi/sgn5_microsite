f8416ac186166cd1cca56196e0a0e148dd91464a
eric_foo@edb.gov.sg <eric_foo@edb.gov.sg>
2024-10-24 10:59:13 +0800

    remove duplicates

--- src/data/restaurants.js
+++ src/data/restaurants.js
@@ -5,40 +5,4 @@ export const restaurantsArr = [
     {
         name: "Ma-Makan", location: "Berlin", promotion: "Free Teh Tarik or Kopi with every 20€ spent", condition: "Redeemable once per customer.", terms: "Valid till", validity: "31 December 2024", image: "/makan-party/mamakanberlin-6x4.jpg", link: "https://www.instagram.com/mamakanberlin/?hl=en"
     },
-    {
-        name: "Cafe Singapura", location: "Brazil", promotion: "Free Virgin Singapore Sling with Rainbow Jelly (500ml)", condition: "Only for dine-ins. Follow our IG page to redeem.", terms: "Valid till", validity: "31 December 2024", image: "/makan-party/cafesingapura-6x4.jpg", link: "https://www.instagram.com/cafe.singapura/"
-    },
-    {
-        name: "Serai", location: "Chicago", promotion: "10% off your dining bill", terms: "Valid till", validity: "31 December 2024", image: "/makan-party/Serai-6x4.jpg", link: "https://www.yelp.com/reservations/serai-chicago?source=yelp_biz"
-    },
-    {
-        name: "Kapitan", location: "Chicago", promotion: "10% off your dining bill", terms: "Valid till", validity: "31 December 2024", image: "/makan-party/Kapitan-6x4.jpg", link: "https://www.yelp.com/reservations/kapitan-chicago-2?source=yelp_biz"
-    },
-    {
-        name: "Cafe Malacca", location: "Hong Kong", promotion: "10% off A La Carte Menu", terms: "Valid till", validity: "31 December 2024", image: "/makan-party/cafemalacca-6x4.jpg", link: "https://inline.app/booking/CafeMalacca/JENHongKong"
-    },
-    {
-        name: "Singlish NYC", location: "New York", promotion: "Free Kueh Pie Tee dish with any drinks ordered", terms: "Valid till ", validity: "30 November 2024", image: "/makan-party/Singlish-6x4.jpg", link: "https://resy.com/cities/new-york-ny/venues/singlish?date=2024-09-18&seats=2"
-    },
-    {
-        name: "THE HOOD PARIS", location: "Paris", promotion: "Free slice of Pandan Kaya for every review!", terms: "Valid till ", validity: "15 November 2024", image: "/makan-party/Thehoodparis-6x4.jpg", link: "www.thehoodparis.com/reservations"
-    },
-    {
-        name: "NONETTE Bánh mì & Donuts", location: "Paris", promotion: "Free Kaya Donut for every review", terms: "Valid till ", validity: "31 December 2024", image: "/makan-party/Nonette-6x4.jpg", link: "https://www.instagram.com/nonettebanhmi/?hl=en"
-    },
-    {
-        name: "Makan Madeira", location: "Portugal", promotion: "10% off your dining bill with a min. spend of 50€", terms: "Valid till ", validity: "29 December 2024", image: "/makan-party/Makan-madeira-6x4.jpg", link: "https://g.co/kgs/yMPEh3o"
-    },
-    {
-        name: "Dabao Singapore", location: "San Francisco", promotion: "One free curry puff for every $20 spent", condition: "Redeemable once per customer.", terms: "Valid till ", validity: "31 December 2024", image: "/makan-party/dabaosg-6x4.jpg", link: "https://www.dabaosingapore.com"
-    },
-    {
-        name: "Nectar Southeast Asian Desserts (Rose Garden Branch) Nectar 花蜜·东南亚甜品(玫瑰坊店)", location: "Shanghai", promotion: "10% off regular products.", condition: "Does not apply to discounted items", terms: "Valid till ", validity: "31 December 2024", image: "/makan-party/nectar.jpeg", link: "https://map.baidu.com/poi/Nectar%E8%8A%B1%E8%9C%9C%E7%94%9C%E5%93%81(%E7%8E%AB%E7%91%B0%E5%9D%8A%E5%BA%97)/@13517141.685,3639704.12,17.93z?uid=ad45a703fa47b90ec03644ed&ugc_type=3&ugc_ver=1&device_ratio=2&compat=1&pcevaname=pc4.1&querytype=detailConInfo&da_src=shareurl"
-    },
-    {
-        name: "RUMAH Taipei", location: "Taipei", promotion: "Free Orange Cooler or Apple Spritzer with every purchase of a main course", terms: "Valid till ", validity: "31 December 2024", image: "/makan-party/Rumahtaipei-6x4.jpg", link: "http://rumah.resos.com/booking"
-    },
-    {
-        name: "Grandmama's", location: "Toronto", promotion: "10% off orders above $25", terms: "Valid till ", validity: "30 November 2024", image: "/makan-party/grandmama-6x4.jpg", link: "https://www.instagram.com/grandmamas.ca/"
-    },
 ];
