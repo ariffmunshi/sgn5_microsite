2474c1a6e81d50c6f17bfbf394985c747cd2167c
edb.sinchin <tan_sin_chin@edb.gov.sg>
2025-04-28 17:33:04 +0800

    feature: remove files from previous project in app directory

--- app/src/data/restaurants.js
+++
@@ -1,8 +0,0 @@
-export const restaurantsArr = [
-    {
-        name: "Vanda Room 狮城小馆", location: "Beijing", promotion: "Free Teh Tarik or Kopi with one order of main course.", condition: "Only for dine-ins. Please inform staff before placing your order.", terms: "Valid till ", validity: "31 December 2024", image: "/makan-party/Vandaroom-6x4.jpg", link: "https://www.facebook.com/vandaroombeijing/"
-    },
-    {
-        name: "Ma-Makan", location: "Berlin", promotion: "Free Teh Tarik or Kopi with every 20€ spent", condition: "Redeemable once per customer.", terms: "Valid till", validity: "31 December 2024", image: "/makan-party/mamakanberlin-6x4.jpg", link: "https://www.instagram.com/mamakanberlin/?hl=en"
-    },
-];
