994ce9292ba78233f2b9a72233f9dce2acd7fc1a
edb.sinchin <tan_sin_chin@edb.gov.sg>
2025-05-05 17:28:38 +0800

    fix: update providers.tf to avoid second role assumption

--- infra/backend/providers.tf
+++ infra/backend/providers.tf
@@ -1,8 +1,9 @@
 provider "aws" {
   region  = "ap-southeast-1"
+
+  # Deployed locally
   profile = "sgn-microsite"
 
-  # No need to assume role when running in GitLab CI
   assume_role {
     role_arn    = "arn:aws:iam::211125747707:role/rol-sgathome_tf"
     session_name = "terraform-local"
--- infra/environments/development/providers.tf
+++ infra/environments/development/providers.tf
@@ -18,12 +18,15 @@ terraform {
   }
 }
 
+
 provider "aws" {
   region  = "ap-southeast-1"
-  profile = "sgn-microsite"
 
-  assume_role {
-    role_arn     = "arn:aws:iam::211125747707:role/rol-sgathome_tf"
-    session_name = "terraform-local-development"
-  }
+	# Only needed for local development
+#   profile = "sgn-microsite"
+
+#   assume_role {
+#     role_arn     = "arn:aws:iam::211125747707:role/rol-sgathome_tf"
+#     session_name = "terraform-local-development"
+#   }
 }
