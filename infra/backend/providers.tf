b361a21769b5ab6e758b5bb7ae524e2c4bf90959
edb.sinchin <tan_sin_chin@edb.gov.sg>
2025-05-05 15:55:58 +0800

    ci: update AWS profile names for all environments

--- infra/backend/providers.tf
+++ infra/backend/providers.tf
@@ -1,7 +1,8 @@
 provider "aws" {
   region  = "ap-southeast-1"
-  profile = "sgn-aws" # This profile is used for local development
+  profile = "sgn-microsite"
 
+  # No need to assume role when running in GitLab CI
   assume_role {
     role_arn    = "arn:aws:iam::211125747707:role/rol-sgathome_tf"
     session_name = "terraform-local"
