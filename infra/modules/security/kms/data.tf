465ab79a73139f7942fc273d46bd6465ac3d66a3
edb.sinchin <tan_sin_chin@edb.gov.sg>
2025-05-09 18:04:14 +0800

    fix: resolve error with KMS module setup

--- infra/modules/security/kms/data.tf
+++ infra/modules/security/kms/data.tf
@@ -1,35 +1,41 @@
-# Policy document to allow CloudWatch to use the KMS key
 data "aws_iam_policy_document" "cloudwatch_kms_policy" {
+  # Enable root user permissions
   statement {
-    sid    = "Enable IAM User Permissions"
+    sid    = "Enable Root User Permissions"
     effect = "Allow"
     principals {
-      type        = "AWS"
-      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
+      type = "AWS"
+      identifiers = [
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root",
+        data.aws_caller_identity.current.arn
+      ]
     }
-    actions   = ["kms:*"]
+    actions = [
+      "kms:*"
+    ]
     resources = ["*"]
   }
 
+  # CloudWatch Logs permissions
   statement {
-    sid    = "AllowCloudWatchLogsToUseKey"
+    sid    = "Allow CloudWatch Logs to encrypt logs"
     effect = "Allow"
     principals {
       type        = "Service"
       identifiers = ["logs.${var.region}.amazonaws.com"]
     }
     actions = [
-      "kms:Encrypt*",
-      "kms:Decrypt*",
+      "kms:Encrypt",
+      "kms:Decrypt",
       "kms:ReEncrypt*",
       "kms:GenerateDataKey*",
-      "kms:Describe*"
+      "kms:DescribeKey"
     ]
     resources = ["*"]
     condition {
-      test     = "ArnEquals"
+      test     = "ArnLike"
       variable = "kms:EncryptionContext:aws:logs:arn"
-      values   = ["arn:aws:logs:${var.region}:${data.aws_caller_identity.current.account_id}:log-group:/aws/cloudtrail/${var.project_name}-${var.env}-cloudtrail"]
+      values   = ["arn:aws:logs:${var.region}:${data.aws_caller_identity.current.account_id}:*"]
     }
   }
 }
