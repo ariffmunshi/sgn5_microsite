146375fe57b7f190f89e3ab8ddb81ec1c1679068
edb.sinchin <tan_sin_chin@edb.gov.sg>
2025-05-09 20:06:55 +0800

    fix: update KMS policy

--- infra/modules/security/kms/data.tf
+++ infra/modules/security/kms/data.tf
@@ -7,7 +7,7 @@ data "aws_iam_policy_document" "cloudwatch_kms_policy" {
       type = "AWS"
       identifiers = [
         "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root",
-        data.aws_caller_identity.current.arn
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/rol-microsite_tf"
       ]
     }
     actions = [
