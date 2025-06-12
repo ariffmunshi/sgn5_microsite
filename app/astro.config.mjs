298004bb472e076661b388b13958031e7e4415f9
Goh Zhen Rong <goh_zhen_rong@edb.gov.sg>
2025-06-13 00:58:33 +0800

    Web Version for Quiz

--- app/astro.config.mjs
+++ app/astro.config.mjs
@@ -1,8 +1,20 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import lottie from "astro-integration-lottie";
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+  site: 'https://example.com',
+  integrations: [
+    lottie(),
+  ],
+  vite: { // Add this vite configuration block for wsl2 support
+    server: {
+      watch: {
+        usePolling: true, // This enables polling for file changes
+      },
+      fs: {
+        allow: ['..'], // Allow serving files from parent directory (includes node_modules)
+      },
+    },
+  },
 });
-
---
+++ app/src/questions.js
@@ -0,0 +1,61 @@
+
+
+export const quizQuestions = [
+    {
+        questionText: "How do you usually order food at the hawker centre?",
+        options: ["I just queue and give the owners a nod - they already know my usual",
+                "I dabao ahead on the app, and skip the queue to collect",
+                "I order a delivery straight to my house"],
+        lottieFileUrl: "lottie/Questions Page/Q1 Scene.json"
+    },
+    {
+        questionText: "Your ideal weekend includes...",
+        options: ["A morning walk by East Coast, then kopi with my family",
+            "Cafe-hopping, people-watching, and updating my socials",
+            "Bouncing between community events or volunteering"],
+        lottieFileUrl: "lottie/Questions Page/Q2 Scene.json"
+    },
+    {
+        questionText: "You're working on a group project and hit a bump. You ...",
+        options: ["Take the strategic approach- assign tasks based on everyone's strengths.",
+            "Have an open conversation to hear everyone out before solving it together",
+            "Start by breaking the tension - crack a few jokes to lighten the mood."],
+        lottieFileUrl: "lottie/Questions Page/Q3 Scene.json"
+    },
+    {
+        questionText: "What music gets you in the National Day spirit?",
+        options: ["Jamming to 'OOs hits by artists like Kit Chan and Taufik Batisah on YouTube",
+            "Listening to lo-fi NDP beats on Spotify",
+            "Tuning into old-school 80s radio tunes, or listening to mixtapes on my Walkman"],
+        lottieFileUrl: "lottie/Questions Page/Q4 Scene.json"
+    },
+    {
+        questionText: "What's your attitude towards technology and innovation?",
+        options: ["If it makes life easier, I'm all for it. Bring on automation!",
+            "I'm tech-capable, but I'm not running after every new trend",
+            "Honestly, I'm happy with how things are. No need to change what works!"],
+        lottieFileUrl: "lottie/Questions Page/Q5 Scene.json"
+    },
+    {
+        questionText: "At National Day Parade, you're most excited about ...",
+        options: ["The Red Lions' freefall - shiok to watch every year!",
+            "Singing along to 'This is Home, truly ... ' with everyone",
+            "Sharing parade highlights online, and keeping an eye out for this year's NDP meme"],
+        lottieFileUrl: "lottie/Questions Page/Q6 Scene.json"
+    },
+    {
+        questionText: "The trip finally made it out of the group chat! How do you prep for the trip?",
+        options: ["I book everything in advance and plan a full itinerary.",
+            "I go with the flow - I bring the energy and good vibes",
+            "I split the planning, suggesting unique local spots and hidden gems"],
+        lottieFileUrl: "lottie/Questions Page/Q7 Scene.json"
+    },
+    {
+        questionText: "What's most important to you in life?",
+        options: ["Stability and achieving life milestones like housing, career and family",
+            "Doing something meaningful and original, even if it's unconventional",
+            "Making a positive impact, especially for future generations"],
+        lottieFileUrl: "lottie/Questions Page/Q8 Scene.json"
+    },
+
+]
