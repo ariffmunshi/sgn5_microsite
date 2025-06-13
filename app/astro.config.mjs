0f893bae02180d23e050621d534cf4fc4d9214e6
Goh Zhen Rong <goh_zhen_rong@edb.gov.sg>
2025-06-13 15:45:59 +0800

    Cleaned up unneeded files

--- app/astro.config.mjs
+++ app/astro.config.mjs
@@ -1,19 +1,18 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import lottie from "astro-integration-lottie";
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [
-    lottie(),
-  ],
-  vite: { // Add this vite configuration block for wsl2 support
+  site: "https://example.com",
+  integrations: [lottie()],
+  vite: {
+    // Add this vite configuration block for wsl2 support
     server: {
       watch: {
         usePolling: true, // This enables polling for file changes
       },
       fs: {
-        allow: ['..'], // Allow serving files from parent directory (includes node_modules)
+        allow: [".."], // Allow serving files from parent directory (includes node_modules)
       },
     },
   },
